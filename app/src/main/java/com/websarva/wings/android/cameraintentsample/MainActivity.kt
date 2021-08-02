a6c11ea788f31c0737f3fe19e108151cd7647e98
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-08-02 17:13:52 +0900

    ストレージの利用

--- app/src/main/java/com/websarva/wings/android/cameraintentsample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/cameraintentsample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.websarva.wings.android.cameraintentsample
 import android.content.Intent
 import android.graphics.Bitmap
 import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,6 +11,7 @@ import android.view.View
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private var _imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,9 +20,9 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == 200 && resultCode == RESULT_OK){
-            val bitmap = data?.getParcelableExtra<Bitmap>("data")
+
             val ivCamera = findViewById<ImageView>(R.id.ivCamera)
-                    ivCamera.setImageBitmap(bitmap)
+                    ivCamera.setImageURI(_imageUri)
         }
     }
 
